9d17030d0d33bdfa1dae60e994365623f03898c5
Marcus <marcus.kalmar@gmail.com>
2019-11-21 14:23:39 +0100

    Add more stats

--- src/routes/stats/controllers.js
+++ src/routes/stats/controllers.js
@@ -2,12 +2,21 @@ const { pool } = require('../../../config')
 
 const getAllStats = async (request, response) => {
     try {
-        const characters = await pool.query('SELECT COUNT (id) FROM characters')
-        const userCount = await pool.query('SELECT COUNT (id) FROM users')
-        console.log(characters, userCount)
+        const characters = await pool.query('SELECT COUNT (id) as characterCount FROM characters')
+        const userCount = await pool.query('SELECT COUNT (id) as userCount FROM users')
+        const heritageDistribution = await pool.query('SELECT COUNT (cultural_heritage), cultural_heritage FROM characters GROUP BY cultural_heritage')
+        const archetypeDistribution = await pool.query('SELECT COUNT(archetype), archetype FROM characters GROUP by archetype')
+        const backgroundDistribution = await pool.query('SELECT COUNT(background), background FROM characters GROUP by background')
+        
+        const stats = [
+            characters.rows, 
+            userCount.rows, 
+            heritageDistribution.rows, 
+            archetypeDistribution.rows, 
+            backgroundDistribution.rows
+        ]
 
-
-        response.status(200).send('PLACEHOLDER')
+        response.status(200).send(stats)
     } catch (error) {
         response.status(400).send('Something went wrong!')
     }
